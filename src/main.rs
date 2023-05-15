6a66f930f183c179151493ce25cda1ed2a0df10c
Mikael <mikael.hatanpaa@protonmail.com>
2023-05-15 17:32:07 +0200

    Finalisation of minigrep

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use minigrep::{run, Config};
+use std::{env, process};
+
+fn main() {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Aplication error: {}", e);
+        process::exit(1);
+    }
+}
